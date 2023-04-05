e2637001b18b45c214ed24c91fdcaec2a3a6f759
hodanov <1031hoda@gmail.com>
2023-04-05 10:08:07 +0900

    Update config to use eslint and prettier.

--- config/lua/null_ls.lua
+++ config/lua/null_ls.lua
@@ -22,12 +22,8 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.shfmt,
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        -- null_ls.builtins.completion.spell,
-        -- null_ls.builtins.diagnostics.golangci_lint,
         null_ls.builtins.formatting.goimports,
-        -- null_ls.builtins.diagnostics.markdownlint_cli2,
-        -- null_ls.builtins.formatting.markdownlint,
+        null_ls.builtins.formatting.prettier,
     },
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
