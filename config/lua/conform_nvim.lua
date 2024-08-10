d42f5315d02fc429bb166022b5d87c53ed08b661
hodanov <1031hoda@gmail.com>
2024-08-10 14:40:17 +0900

    Add 'ruff_organize_import'. Update README.

--- config/lua/conform_nvim.lua
+++ config/lua/conform_nvim.lua
@@ -7,6 +7,6 @@ require("conform").setup({
 		go = { "goimports" },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 		lua = { "stylua" },
-		python = { "ruff_fix", "ruff_format" },
+		python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
 	},
 })
