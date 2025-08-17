5efd30907b108e6382d631a335ccaa4252f907e3
hodanov <14236600+hodanov@users.noreply.github.com>
2025-08-18 00:17:25 +0900

    Repair treesitter temporarily.

--- config/lua/nvim_treesitter.lua
+++ config/lua/nvim_treesitter.lua
@@ -1,7 +1,15 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "go", "python" },
+	ensure_installed = { "go", "python", "markdown", "markdown_inline" },
 	highlight = {
 		enable = true,
 		-- disable = { "go", "python" },
+		-- TODO: neovim 0.11.4リリース後にコメントアウトする。
+		-- markdown編集時に下記のエラーが発生するため一時的に無効にしている。
+		-- Error executing lua: ...al/share/nvim/runtime/lua/vim/treesitter/highlighter.lua:370: Invalid 'end_col': out of range
+		-- 関連issue: https://github.com/neovim/neovim/issues/29550
+		disable = function(lang, _)
+			return lang == "markdown" or lang == "markdown_inline"
+		end,
+		additional_vim_regex_highlighting = { markdown = true },
 	},
 })
