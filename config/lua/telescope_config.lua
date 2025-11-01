fc67a09ffd532cf01135deb455363f9f2bf2a9b3
sakamoto-hodaka <14236600+hodanov@users.noreply.github.com>
2025-11-01 15:04:58 +0900

    Modify to show hidden files using by telescope.nvim.

--- config/lua/telescope_config.lua
+++ config/lua/telescope_config.lua
@@ -2,8 +2,24 @@
 -- telescope.nvim
 -- ----------
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<Leader>ff", builtin.find_files, { desc = "Find Files" })
-vim.keymap.set("n", "<Leader>fg", builtin.live_grep, { desc = "Live Grep" })
+-- vim.keymap.set("n", "<Leader>ff", builtin.find_files, { desc = "Find Files" })
+-- vim.keymap.set("n", "<Leader>fg", builtin.live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<Leader>ff", function()
+	builtin.find_files({
+		hidden = true, -- .git, .github, .configなどを含む
+		no_ignore = true, -- .gitignoreを無視しない
+		no_ignore_parent = true,
+	})
+end, { desc = "Find Files (include hidden)" })
+
+vim.keymap.set("n", "<Leader>fg", function()
+	builtin.live_grep({
+		additional_args = function()
+			return { "--hidden" } -- grepでも隠しファイルを対象に
+		end,
+	})
+end, { desc = "Live Grep (include hidden)" })
+
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
@@ -20,6 +36,13 @@ telescope.setup({
 		},
 		find_files = { hidden = true },
 	},
+	pickers = {
+		find_files = {
+			hidden = true,
+			no_ignore = true,
+			no_ignore_parent = true,
+		},
+	},
 	extensions = {
 		file_browser = {
 			sorting_strategy = "ascending",
@@ -27,6 +50,7 @@ telescope.setup({
 				prompt_position = "top",
 			},
 			grouped = true, -- ディレクトリを上にまとめる
+			hidden = true,
 		},
 	},
 })
@@ -36,5 +60,7 @@ vim.keymap.set("n", "<space>fo", function()
 	require("telescope").extensions.file_browser.file_browser({
 		path = "%:p:h", -- 現在開いてるファイルのディレクトリ
 		select_buffer = true, -- バッファのパスを基準に
+		hidden = true,
 	})
 end, { desc = "Telescope File Browser (current file dir)" })
+
